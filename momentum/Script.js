a3eef46a4e416cf9279235e6fcb60b4a740cc54c
sikdong <zmstnv1119@naver.com>
2022-09-06 20:47:29 +0900

    노마드 코더 0906

--- momentum/Script.js
+++ momentum/Script.js
@@ -1,15 +1,30 @@
-const h1 = document.querySelector("div.hello:first-child h1")
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-
-function handleTitleClick(){
-const clickedClass = "active";
-if(h1.className === clickedClass){
-    h1.className = "";
-} else {
-    h1.className = clickedClass;
-};
+function onLoginSubmit(event){
+    event.preventDefault();
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   localStorage.setItem(USERNAME_KEY, loginInput.value);
+   paintGreetings();
 }
 
-h1.addEventListener("click", handleTitleClick);
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+loginForm.addEventListener("submit", onLoginSubmit);
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+    }else {
+        paintGreetings();
+    }
